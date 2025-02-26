facc389c34fc47446a8fb5f020ae8ccfa49daf6c
lyonva <lyon.villalobos@gmail.com>
2025-02-26 18:49:24 -0500

    Adds GRM delay to run scripts

--- runs.bat
+++ runs.bat
@@ -1,7 +1,7 @@
-SET map=Empty-16x16
-SET im=NoModel
+SET map=Doorkey-16x16
+SET im=RND
 SET seeds=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 (for %%s in %seeds% do (
-    python train.py --env_source=minigrid --game_name=%map% --model_features_dim=64 --int_rew_source=%im% --use_wandb=1 --run_id=%%s
+    python train.py --env_source=minigrid --game_name=%map% --model_features_dim=64 --int_rew_source=%im% --use_wandb=1 --run_id=%%s --grm_delay=1
 ))
--- runs.sh
+++ runs.sh
@@ -4,5 +4,5 @@ im="NoModel"
 seeds=(1 2 3 4 5 6 7 8 9 10)
 
 for seed in ${seeds[@]}; do
-    python train.py --env_source=minigrid --game_name="$map" --model_features_dim=64 --int_rew_source="$im" --use_wandb=1 --run_id=$seed
+    python train.py --env_source=minigrid --game_name="$map" --model_features_dim=64 --int_rew_source="$im" --use_wandb=1 --run_id=$seed --grm_delay=1
 done
