6889b56f462029369106b4a2aa7a39cebe9779d9
Leonardo Villalobos-Arias <lyon.villalobos@gmail.com>
2025-04-24 14:20:40 -0400

    Adds script for manually running/controlling minigrid envs

---
+++ minigrid_run_env.py
@@ -0,0 +1,11 @@
+from minigrid.manual_control import ManualControl
+from minigrid.minigrid_env import MiniGridEnv
+import gymnasium as gym
+import minigrid
+
+# env = gym.make("MiniGrid-DoorKey-6x6-v0",render_mode="human")
+env = gym.make("MiniGrid-FourRooms-v0",render_mode="human")
+
+# enable manual control for testing
+manual_control = ManualControl(env)
+manual_control.start()
