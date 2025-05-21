83bf0dd9252faefd92fbf0099fc7d0b64fa2ebdd
Leonardo Villalobos-Arias <lyon.villalobos@gmail.com>
2025-05-21 15:07:00 -0400

    Update batch running scripts.

--- runs.bat
+++ runs.bat
@@ -1,8 +1,9 @@
 SET map=Empty-16x16
 SET im=NoModel
 SET irc=0.0005
+SET rs=NoRS
 SET seeds=(1, 2, 3, 4, 5)
 
 (for %%s in %seeds% do (
-    python train.py --game_name=%map% --model_features_dim=64 --int_rew_source=%im% --int_rew_coef=%irc% --run_id=%%s --grm_delay=1 --model_recs=[50,100,250,500,1000]
+    python train.py --game_name=%map% --model_features_dim=64 --int_rew_source=%im% --int_rew_coef=%irc% --run_id=%%s --int_shape_source=%rs% --grm_delay=1 --model_recs=[50,100,250,500,1000]
 ))
--- runs.sh
+++ runs.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-map="DoorKey-16x16"
+map="Empty-16x16"
 im="NoModel"
-icm=0.01
+irc=0.0005
+rs="NoRS"
 seeds=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
 
 for seed in ${seeds[@]}; do
-    python train.py --game_name="$map" --model_features_dim=64 --int_rew_source="$im" --int_rew_coef=$icm --use_wandb=1 --run_id=$seed --grm_delay=1 --model_recs=[3,15,305,610,915,1221]
+    python train.py --game_name="$map" --model_features_dim=64 --int_rew_source="$im" --int_rew_coef=$irc --run_id=$seed --int_shape_source="$rs" --grm_delay=1 --model_recs=[50,100,250,500,1000]
 done
