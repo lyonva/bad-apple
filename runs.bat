17a93475022c1c1cdf60905f91c11033dd7c183d
Leonardo Villalobos-Arias <lyon.villalobos@gmail.com>
2025-06-27 14:14:00 -0400

    Adds more seeds to runs.bat

--- runs.bat
+++ runs.bat
@@ -2,7 +2,7 @@ SET map=RedBlueDoors-8x8
 SET rs=GRM
 SET im=StateCount
 SET irc=0.05
-SET seeds=(1)
+SET seeds=(1,2,3)
 
 (for %%s in %seeds% do (
     python train.py --game_name=%map% --int_rew_source=%im% --int_rew_coef=%irc% --run_id=%%s --int_shape_source=%rs% --grm_delay=1 --model_recs=[250,500,1250,2500,5000]
