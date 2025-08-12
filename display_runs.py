3fb35d822f4c3bf8feb4eb46863d96b3944e5061
Leonardo Villalobos-Arias <lyon.villalobos@gmail.com>
2025-08-12 18:26:46 -0400

    Update to include 2 new maps to display_runs.

--- display_runs.py
+++ display_runs.py
@@ -24,6 +24,8 @@ snap_dict = {
     "MiniGrid-DoorKey-8x8-v0" : [500,1000,2500,5000,10000],
     "MiniGrid-RedBlueDoors-8x8-v0" : [500,1000,2500,5000,10000],
     "MiniGrid-FourRooms-v0" : [1250,2500,6250,12500,25000],
+    "MiniGrid-LavaCrossingS11N5-v0" : [500,1000,2500,5000,10000],
+    
 }
 default_snaps = [250,500,1250,2500,5000]
 
