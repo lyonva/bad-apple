fd956ec90a95e0a8f10e0465745e08f91cad2be1
Leonardo Villalobos-Arias <lyon.villalobos@gmail.com>
2025-08-13 14:54:06 -0400

    Adds MultiRoom medium to display_runs

--- display_runs.py
+++ display_runs.py
@@ -25,7 +25,7 @@ snap_dict = {
     "MiniGrid-RedBlueDoors-8x8-v0" : [500,1000,2500,5000,10000],
     "MiniGrid-FourRooms-v0" : [1250,2500,6250,12500,25000],
     "MiniGrid-LavaCrossingS11N5-v0" : [500,1000,2500,5000,10000],
-    
+    "MiniGrid-MultiRoom-N4-S5-v0" : [500,1000,2500,5000,10000],
 }
 default_snaps = [250,500,1250,2500,5000]
 
