776abf771146d089e8946239645971366b208013
lyonva <lyon.villalobos@gmail.com>
2026-05-13 12:13:49 -0400

    Updates parameters in run_safe.bat

--- run_safe.bat
+++ run_safe.bat
@@ -1,10 +1,10 @@
 SET map=SafeWaterMaze-5x7
-SET cost_limit=2.5
+SET cost_limit=3
 
-REM SET cost_objective="NoCO"
+@REM SET cost_objective="NoCO"
 SET cost_objective="Lag"
-REM SET cost_objective="SB"
-REM SET cost_objective="SaBER"
+@REM SET cost_objective="SB"
+@REM SET cost_objective="SaBER"
 
 SET im="RND"
 SET rs="ADOPS"
@@ -12,12 +12,12 @@ SET rs="ADOPS"
 REM SafeWaterMaze-5x7
 SET steps=4096000
 SET recs=19,37,75,125,250,500
-SET irc=0.0025
+SET irc=0.001
 SET norm=1
-SET pies_decay=350
-SET lagrange_learning_rate=0.01
+SET pies_decay=250
+SET lagrange_learning_rate=0.02
 SET saber_zeta_min_rollout=1
-SET saber_zeta_max_rollout=150
+SET saber_zeta_max_rollout=125
 
 SET seeds=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
 
